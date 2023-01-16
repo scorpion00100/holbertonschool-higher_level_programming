833c65a76d019b51a211716f9e2db1b61335008c
scorpion00100 <danbetheliryivuze@gmail.com>
2023-01-16 21:16:32 +0100

    function that prints the numbers from 1 to 100

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for n in range(1, 100 + 1):
+        if n % 3 == 0 and n % 5 == 0:
+            msg = "FizzBuzz"
+        elif n % 3 == 0:
+            msg = "Fizz"
+        elif n % 5 == 0:
+            msg = "Buzz"
+        else:
+            msg = str(n)
+
+        print("{:s}".format(msg), end=' ')
