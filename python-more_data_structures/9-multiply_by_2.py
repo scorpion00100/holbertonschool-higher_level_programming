75cee9d2d9d16bff3c310dd1360fc6fdae7730da
scorpion00100 <danbetheliryivuze@gmail.com>
2023-01-24 23:38:50 +0100

    returns a new dictionary with all values multiplied by 2

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for i in sorted(a_dictionary):
-        return 2 * a_dictionary[i]
+      return({i: 2 * a_dictionary[i] for i in a_dictionary})
