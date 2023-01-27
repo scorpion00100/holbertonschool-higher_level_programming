c08cee0146c21245c7341f23be670ccc06c300fd
scorpion00100 <danbetheliryivuze@gmail.com>
2023-01-27 14:36:35 +0100

    divides element by element 2 lists

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -6,7 +6,7 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             div = my_list_1[i] / my_list_2[i]
         except (ZeroDivisionError, ValueError):
-            print("div by 0")
+            print("division by 0")
         except (TypeError):
             print("wrong type")
         except (IndexError):
