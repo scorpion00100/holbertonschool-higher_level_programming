66d4463800018712468c11056dabad568ad3efe0
scorpion00100 <danbetheliryivuze@gmail.com>
2023-01-31 09:47:32 +0100

    class Square that defines a square by: (based on 2-square.py)

---
+++ python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Defines class Square with private attribute size and validates size
+"""
+
+
+class Square:
+    """
+    size (int): size of a side in square
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return (self.__size)**2
