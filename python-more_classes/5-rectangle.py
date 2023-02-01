45d34e8bb34079e1342bea7151895f872725eb3a
scorpion00100 <danbetheliryivuze@gmail.com>
2023-02-01 15:41:27 +0100

    class Rectangle that defines a rectangle by: (based on 4-rectangle.py)

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -8,6 +8,20 @@ public area and perimeter methods, allows printing #'s, and deletes
 class Rectangle:
     """
     Defines a class rectangle with private attribute width and height
+    Args:
+        width (int): width
+        height (int): height
+    Functions:
+        __init__(self, width, height)
+        width(self)
+        width(self, value)
+        height(self)
+        height(self, value)
+        area(self)
+        perimeter(self)
+        __str__(self)
+        __repr__(self)
+        __del__(self)
     """
      def __init__(self, width=0, height=0):
         """ Initialize rectangles """
