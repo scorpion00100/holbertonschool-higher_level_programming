25a70a29d8f61562009145ef6d1fcb038b6b5473
scorpion00100 <danbetheliryivuze@gmail.com>
2023-02-14 02:27:01 +0100

    Update the class Base by adding the static method def to_json_string(list_dictionaries)

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -4,41 +4,23 @@ import json
 
 
 class Base:
-    """private class attribute"""
+    """Parent class in the project"""
+
+    # Number objects created
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """constructor for base class"""
-        if id is not None:
-            self.id = id
-        else:
+        """Constructor Base class"""
+        if id is None:
             Base.__nb_objects += 1
-            self.id = Base.__nb_objectis
+            id = Base.__nb_objects
+
+        self.id = id
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+        """Convert dictionary into json string"""
+        if not list_dictionaries:
             return "[]"
-        else:
-            return json.dumps(list_dictionaries)
 
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        """Save a list of object in a json file"""
-        dicts = []
-        if list_objs:
-            for ins in list_objs:
-                dicts.append(cls.to_dictionary(ins))
-
-        json_string = Base.to_json_string(dicts)
-        with open(cls.__name__ + '.json', 'w') as f:
-            f.write(json_string)
-
-    @staticmethod
-    def from_json_string(json_string):
-        """Return list Pyhton objects from json_string"""
-        if not json_string:
-            return []
-
-        return json.loads(json_string)
+        return json.dumps(list_dictionaries)
