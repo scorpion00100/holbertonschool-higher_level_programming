48ed607d10124c264e137c6828297b79687be659
scorpion00100 <danbetheliryivuze@gmail.com>
2023-04-10 17:24:10 +0200

    fixed bug for console

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -4,12 +4,11 @@
 let x = parseInt(process.argv[2]);
 
 if (isNaN(x)) {
-  console.log("Missing number of occurrences");
+  console.log('Missing number of occurrences');
 } else {
   let output = "";
   for (let i = 0; i < x; i++) {
-    output += "C is fun\n";
+    output += 'C is fun\n';
   }
   console.log(output.trim());
 }
-
