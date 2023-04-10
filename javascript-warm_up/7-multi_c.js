dbbbf109644cab0d26971c9d1981cf0565f1a923
scorpion00100 <danbetheliryivuze@gmail.com>
2023-04-10 17:18:44 +0200

    script that prints x times “C is fun”

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// script that prints x times
+
+const arg = process.argv[2];
+
+if (!arg) {
+  console.log("Missing number of occurrences");
+} else {
+  const x = parseInt(arg);
+
+  if (isNaN(x)) {
+    console.log("Missing number of occurrences");
+  } else {
+    for (let i = 0; i < x; i++) {
+      console.log("C is fun");
+    }
+  }
+}
