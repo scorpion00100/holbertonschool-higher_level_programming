69da4b0087f81e46ebddcdb9fcd0cf651f4deaae
scorpion00100 <danbetheliryivuze@gmail.com>
2023-04-10 17:52:36 +0200

    print addition of 2 integers

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
-function add (a, b) {
-    return a + b;
+
+function add(a, b) {
+  return a + b;
 }
 
-console.log(add(Number(process.argv[2]), Number(process.argv[3])));
+const num1 = Number(process.argv[2]);
+const num2 = Number(process.argv[3]);
+
+console.log(add(num1, num2));
