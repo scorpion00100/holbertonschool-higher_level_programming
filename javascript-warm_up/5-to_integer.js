c2e4f8a269c6f9e3a68f0921f0346ac1969f7c62
scorpion00100 <danbetheliryivuze@gmail.com>
2023-04-10 17:09:02 +0200

    fixed again bug

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 // an integer
 
-if (isNaN(parseInt(process.argv[2]))) {
-    console.log('Not a number');
-  } else {
-    console.log('My number: ' + parseInt(process.argv[2]));
-  }
+const num = parseInt(process.argv[2]);
+if (!isNaN(num)) {
+  console.log(`My number: ${num}`);
+} else {
+  console.log("Not a number");
+}
