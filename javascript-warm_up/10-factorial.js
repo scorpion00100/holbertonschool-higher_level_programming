df85fdb5ea9c7f9531ffdacfb727c1741417529f
scorpion00100 <danbetheliryivuze@gmail.com>
2023-04-10 18:02:28 +0200

    fixed bug semistand

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,6 +1,16 @@
 #!/usr/bin/node
 
-function factorial (n) {
-    return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
-}
-console.log(factorial(Number(process.argv[2])));
+function factorial(n) {
+    if (isNaN(n)) {
+      return 1;
+    }
+    if (n === 0) {
+      return 1;
+    } else {
+      return n * factorial(n - 1);
+    }
+  }
+  
+  const num = Number(process.argv[2]);
+  console.log(factorial(num));
+  
