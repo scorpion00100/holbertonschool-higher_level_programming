b0bfdeb8f24a2032ddd62456c731b664486c0f55
scorpion00100 <danbetheliryivuze@gmail.com>
2023-04-10 17:30:34 +0200

    fixed bug semistand

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 if (isNaN(parseInt(process.argv[2]))) {
     console.log('Missing number of occurrences');
-  } else {
+} else {
     for (let i = 0; i < parseInt(process.argv[2]); i++) {
       console.log('C is fun');
     }
-  }
+}
