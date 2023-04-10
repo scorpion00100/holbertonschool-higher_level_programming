c26a3203d3662af5beee9c5b957ff2da63399f11
scorpion00100 <danbetheliryivuze@gmail.com>
2023-04-10 17:48:18 +0200

    script that prints the addition of 2 integers

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -2,7 +2,7 @@
 
 function add(a, b) {
     return a + b;
-  }
+}
   
-  console.log(add(Number(process.argv[2]), Number(process.argv[3])));
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
   
