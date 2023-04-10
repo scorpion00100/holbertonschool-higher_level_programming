d626f86410c2c05b76e195fe9d2045dc77287f17
scorpion00100 <danbetheliryivuze@gmail.com>
2023-04-10 17:32:37 +0200

    fixed bug again smistand

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
-if (isNaN(parseInt(process.argv[2]))) {
-    console.log('Missing number of occurrences');
+
+const i = 0;
+if (!process.argv[2]) {
+  console.log('Missing number of occurrences');
 } else {
-    for (let i = 0; i < parseInt(process.argv[2]); i++) {
-      console.log('C is fun');
-    }
+  for (i = 0; i < process.argv[2]; i++) {
+    console.log('C is fun');
+  }
 }
