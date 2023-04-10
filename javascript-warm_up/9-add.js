7cbed3644214536c20c86438ce62230d9c6780d2
scorpion00100 <danbetheliryivuze@gmail.com>
2023-04-10 17:47:34 +0200

    fixed semistand

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
-function add (a, b) {
+
+function add(a, b) {
     return a + b;
-}
-console.log(add(Number(process.argv[2]), Number(process.argv[3])));
+  }
+  
+  console.log(add(Number(process.argv[2]), Number(process.argv[3])));
+  
