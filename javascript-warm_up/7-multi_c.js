b2e951c982a009485c3486cf0607cce9a1863c95
scorpion00100 <danbetheliryivuze@gmail.com>
2023-04-10 17:25:54 +0200

    fixed again bug

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 // script that prints x times
 
-let x = parseInt(process.argv[2]);
+const x = parseInt(process.argv[2]);
 
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
   let output = "";
   for (let i = 0; i < x; i++) {
-    output += 'C is fun\n';
+    output += 'C is fun';
   }
   console.log(output.trim());
 }
