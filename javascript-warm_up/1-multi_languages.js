8204df9f1bcf1a063a4b3497af6a6f8b7008b1a7
scorpion00100 <danbetheliryivuze@gmail.com>
2023-04-10 16:37:48 +0200

    script that print 3 lines

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// script that print 3 lines
+
+let line1 = 'C is fun';
+let line2 = 'Python is cool';
+let line3 = 'JavaScript is amazing';
+
+console.log(line1);
+console.log(line2);
+console.log(line3);
